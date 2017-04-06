7204ab8ffb817517e955a70a8bb0c48312ff0c7e
mohtisham <mohtishamzubair@gmail.com>
2017-04-06 14:43:26 +0500

    adding admin code

---
+++ publish/admin/Scripts/jquery-demo.js
@@ -0,0 +1,35 @@
+﻿
+(function () {
+    if (typeof enableDemo !== "undefined" && enableDemo){        
+        loadDemoData();        
+    }
+}());
+
+function loadDemoData() {
+
+    var fontSize = $('#appFooter').css('font-size');
+    var checkDataLoad = false;
+    $('#appFooter')
+    	.animate(
+    		{				
+    		    'font-size': 14 
+    		},
+    		(1), function() {
+    		    checkDataLoad = $('#demo').length > 0;
+    		}
+            );
+    $('#appFooter').promise().done(
+    	function () {    	    
+    	    if (checkDataLoad) {
+                callDemoData();
+    	    }
+    	    else {
+    	        loadDemoData();
+            }
+    	});
+}
+
+function callDemoData() {
+    $(".container.body-content").nextAll().not('#demo').appendTo('#demo');
+    $('#demo').css('display', 'none');
+}
